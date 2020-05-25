ac4702a7112806eb48a5bae7844322f2c5a6be63
such8 <mate.hagyak@gmail.com>
2020-05-25 15:33:54 +0200

    introduced logger

--- src/com/complexible/common/csv/CSV2RDF.java
+++ src/com/complexible/common/csv/CSV2RDF.java
@@ -19,6 +19,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -62,6 +64,7 @@ public class CSV2RDF implements Runnable {
 	private static final Charset INPUT_CHARSET = Charset.defaultCharset();
 	private static final Charset OUTPUT_CHARSET = StandardCharsets.UTF_8;
 	private static final ValueFactory FACTORY = ValueFactoryImpl.getInstance();
+	private static final Logger logger = Logger.getLogger(CSV2RDF.class.getName());
 
 	@Option(name = "--no-header", arity = 0, description = "If csv file does not contain a header row")
 	boolean noHeader = false;
@@ -88,10 +91,10 @@ public class CSV2RDF implements Runnable {
 		File templateFile = new File(files.get(0));
 		File inputFile = new File(files.get(1));
 		File outputFile =  new File(files.get(2));
-		System.out.println("CSV to RDF conversion started...");
-		System.out.println("Template: " + templateFile);
-		System.out.println("Input   : " + inputFile);
-		System.out.println("Output  : " + outputFile);
+		logger.log(Level.INFO, "CSV to RDF conversion started...");
+		logger.log(Level.INFO, "Template: {0}", templateFile);
+		logger.log(Level.INFO, "Input   : {0}", inputFile);
+		logger.log(Level.INFO,"Output  : {0}", outputFile);
 		
 		try (Reader in = Files.newReader(inputFile, INPUT_CHARSET);
 			 CSVReader reader = new CSVReader(in, toChar(separator), toChar(quote), toChar(escape));
@@ -120,7 +123,7 @@ public class CSV2RDF implements Runnable {
 		catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		System.out.printf("Converted %,d rows to %,d triples%n", inputRows, outputTriples);
+		logger.log(Level.INFO, "Converted {0} rows to {1} triples%n", new Object[] {inputRows, outputTriples});
 	}
 
 	private static char toChar(String value) {
@@ -429,8 +432,7 @@ public class CSV2RDF implements Runnable {
 			                .build().parse(args).run();
 		}
 		catch (Exception e) {
-			System.err.println("ERROR: " + e.getMessage());
-			e.printStackTrace();
+			logger.log(Level.SEVERE, e.getMessage(), e);
 		}
 	}
 }
